30086d2c8d02093961f153070f8f1a2438a54f51
Peter <schmidt.multimedia@gmail.com>
2022-01-27 09:21:26 +0100

    3. Modul: Business-Logic (Mapping)

--- js/view_controller.js
+++ js/view_controller.js
@@ -5,16 +5,37 @@
     Einlesen Daten von Webseite :: 
 	Check Daten :: 
     Btn. Trigger :: 
-    Business-Logic (Alter --> Getränk) :: 
+    Business-Logic (Alter --> Getränk) :: check!
     Bild austauschen :: check!
 */
 
 /***  Ablaufsteuerung | Control */  
 
-
+/***  Business-Logic */ 
+//3. Modul: Business-Logic (Mapping) | Test:
+output(checkAge(2));
+output(checkAge(7));
+output(checkAge(13));
+output(checkAge(18));
+output(checkAge(99));
+output(checkAge(135));
+function checkAge(age) {
+    switch (true) {
+        case (age >= 0) && (age <= 5) :
+            return "milch";
+        case (age >= 6) && (age <= 12):
+            return "saft";
+        case (age >= 13) && (age <= 17):
+            return "cola";           
+        case (age >= 18) && (age <= 130):
+            return "wein";
+        default:
+            return "tee";           
+    }
+}
 
 /*** View-Schicht aktualisieren */ 
-//1. Modul: Bild aktualisieren | Test:
+//2. Modul: Bild aktualisieren | Test:
 // output(updateImg("cola"));
 // output(updateImg("milch"));
 // output(updateImg("wein"));
