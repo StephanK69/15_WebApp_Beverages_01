7f11e13f9f6e7a068b4756506f4d111233638f0d
Peter <schmidt.multimedia@gmail.com>
2022-01-27 09:07:22 +0100

    1. Modul: Bild aktualisieren

--- js/model.js
+++ js/model.js
@@ -1,3 +1,5 @@
 /* Model (Daten/Logik) */
 
 
+
+
--- js/view_controller.js
+++ js/view_controller.js
@@ -6,19 +6,23 @@
 	Check Daten :: 
     Btn. Trigger :: 
     Business-Logic (Alter --> Getränk) :: 
-    Bild austauschen :: 
+    Bild austauschen :: check!
 */
 
 /***  Ablaufsteuerung | Control */  
 
-/
+
 
 /*** View-Schicht aktualisieren */ 
-
-/
-
-
-
+//1. Modul: Bild aktualisieren | Test:
+// output(updateImg("cola"));
+// output(updateImg("milch"));
+// output(updateImg("wein"));
+function updateImg(imgName) {
+    const img = document.getElementById("bevImg");
+    img.src = "./bilder/" + imgName + ".jpg";
+    return imgName;
+}
 
 //1. Modul: Konsolenausgabe --> Test:
 function output(outputData) {
